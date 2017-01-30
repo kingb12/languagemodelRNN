e6997ab93ef7c0d29ab6b89c9086870ef681afea
kingb12 <kingb12@cs.washington.edu>
2017-01-30 03:34:34 -0800

    fixed eval script for first run

--- evaluation.lua
+++ evaluation.lua
@@ -43,30 +43,54 @@ cmd:option('-run', false)
 
 local opt = cmd:parse(arg)
 -- ================================================ EVALUATION =========================================================
+if opt.gpu then 
+    require 'cutorch'
+    require 'cunn'
+end
 
-train_set = torch.load(opt.train_set)
-valid_set = torch.load(opt.valid_set)
-test_set = torch.load(opt.test_set)
+function clean_dataset(dataset, batch_size)
+    new_set = {}
+    for i=1, #dataset do
+        if dataset[i][1]:size(1) == batch_size then
+            new_set[#new_set + 1] = dataset[i]
+        end
+    end
+    return new_set
+end
+train_set = clean_dataset(torch.load(opt.train_set), 50)
+valid_set = clean_dataset(torch.load(opt.valid_set), 50)
+test_set = clean_dataset(torch.load(opt.test_set), 50)
 model = torch.load(opt.model)
 criterion = nn.ClassNLLCriterion()
 
+function table_cuda(dataset) 
+    for i=1, #dataset do
+        dataset[i][1] = dataset[i][1]:cuda()
+        dataset[i][2] = dataset[i][2]:cuda()
+    end
+    return dataset
+end
+
+
 -- CUDA everything if GPU
 if opt.gpu then
-    require 'cutorch'
-    require 'cunn'
-    train_set = train_set:cuda()
-    valid_set = valid_set:cuda()
-    test_set = test_set:cuda()
+    train_set = table_cuda(train_set)
+    valid_set = table_cuda(valid_set)
+    test_set = table_cuda(test_set)
     model = model:cuda()
     criterion = criterion:cuda()
 end
-
 function loss_on_dataset(data_set, criterion)
     local loss = 0.0
     local batch_losses = {}
-    for i=1, data_set:size() do
+    for i=1, #data_set do
         local l, n = criterion:forward(model:forward(data_set[i][1]), data_set[i][2])
-        batch_losses[i] = l
+        if batch_losses[data_set[i][1]:size(2)] == nil then
+            batch_losses[data_set[i][1]:size(2)] = {l}
+        else 
+            local x = batch_losses[data_set[i][1]:size(2)]
+            x[#x + 1] = l
+        end
         loss = loss + (l / n)
     end
     return loss, batch_losses
@@ -76,14 +100,17 @@ end
 output = {}
 
 -- calculate losses
+print('Calculating Training Loss...')
 local tr_set_loss, tr_batch_loss = loss_on_dataset(train_set, criterion)
 output['train_loss'] = tr_set_loss
 output['train_batch_loss'] = tr_batch_loss
 
+print('Calculating Validation Loss...')
 local vd_set_loss, vd_batch_loss = loss_on_dataset(valid_set, criterion)
 output['valid_loss'] = vd_set_loss
 output['valid_batch_loss'] = vd_batch_loss
 
+print('Calculating Test Loss...')
 local ts_set_loss, ts_batch_loss = loss_on_dataset(test_set, criterion)
 output['test_loss'] = ts_set_loss
 output['test_batch_loss'] = ts_batch_loss
@@ -104,9 +131,4 @@ function sample(output, max_samples)
 end
 
 -- generate some samples
-for i=1, opt.num_samples do
-
-end
-
-
 
