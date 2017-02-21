2da4dcee7c6776c41124160398e7b1040addf27d
Brendan King <kingb12@cs.washington.edu>
2017-02-21 12:00:21 -0800

    eval fixes

--- encdec_eval_functions.lua
+++ encdec_eval_functions.lua
@@ -37,16 +37,46 @@ function sequence_to_string(seq)
     return str
 end
 
+function sample(encoder, decoder, enc_state, sequence, max_samples)
+    if max_samples == nil then
+        max_samples = 1
+    end
+    if enc_state == nil then
+        enc_state = encoder:forward(sequence)
+        sequence = torch.CudaTensor({helper.w_to_n['<beg>']}):reshape(1, 1)
+    end
+    local cb = torch.CudaTensor.zeros(torch.CudaTensor.new(), 1, enc_state:size(3))
+    local addition = torch.zeros(sequence:size(1)):cuda()
+    local output = torch.cat(sequence, addition , 2)
+    local dec_h0 = enc_state[{{}, enc_state:size(2), {}}] -- grab the last hidden state from the encoder, which will be at index max_in_len
+    print(dec_h0:size(), enc_state:size(),cb:size())
+    local y = decoder:forward({cb:clone(), dec_h0, sequence})
+    local sampled = sampler:forward(y)
+    for i=1, output:size(1) do output[i][output:size(2)] = sampled[output:size(2) - 1] end
+    if max_samples == 1 or helper.n_to_w[output[1][output:size(2)]] == '</S>' then
+        return output
+    else
+        return sample(encoder, decoder, enc_state, output, max_samples - 1)
+    end
+end
+
+function sequence_to_string(seq)
+    local str = ''
+    if seq:dim() == 2 then seq = seq[1] end
+    for i=1, seq:size()[1] do
+        local next_word = helper.n_to_w[seq[i]] == nil and '<UNK2>' or helper.n_to_w[seq[i]]
+        str = str..' '..next_word
+    end
+    return str
+end
+
 function generate_samples(data_set, outputs, num_samples)
     local results = {}
-    if opt.max_gen_example_length > 0 then
-        data_set = truncate_dataset(data_set, opt.max_gen_example_length)
-    end
     print('Generating Samples...')
     for i = 1, num_samples do
         print('Sample ', i)
-        local t_set_idx = (torch.random() % #data_set) + 1
-        if t_set_idx > #data_set then t_set_idx = 1 end
+        local t_set_idx = (torch.random() % data_set:size(1)) + 1
+        if t_set_idx > data_set:size(1) then t_set_idx = 1 end
         local example = data_set[t_set_idx]
         local example_no = torch.random() % example:size(1) + 1
         if example_no > example:size(1) then example_no = 1 end
@@ -62,6 +92,7 @@ end
 
 -- calculate perplexity
 function perplexity_over_dataset(enc, dec, enc_inputs, dec_inputs, in_lengths, out_lengths, outputs)
+    local cb = torch.CudaTensor.zeros(torch.CudaTensor.new(), enc_inputs[1]:size(1), enc:forward(enc_inputs[1]):size(3))
     local data_perplexity = 0
     for i=1,enc_inputs:size(1) do
         for _,v in pairs(enc._rnns) do v:resetStates() end
@@ -72,9 +103,9 @@ function perplexity_over_dataset(enc, dec, enc_inputs, dec_inputs, in_lengths, o
         local enc_fwd = enc:forward(enc_input) -- enc_fwd is h1...hN
         local dec_h0 = enc_fwd[{{}, enc_inputs[1]:size(2), {}}] -- grab the last hidden state from the encoder, which will be at index max_in_len
         local dec_fwd = dec:forward({cb:clone(), dec_h0, dec_input}) -- forwarding a new zeroed cell state, the encoder hidden state, and frame-shifted expected output (like LM)
-        dec_fwd = torch.reshape(dec_fwd, opt.batch_size, opt.max_out_len, opt.vocab_size)
+        dec_fwd = torch.reshape(dec_fwd, enc_input:size(1), dec_input:size(2), #helper.n_to_w)
         local loss = criterion:forward(dec_fwd, output) -- loss is essentially same as if we were a language model, ignoring padding
-        loss = loss / (out_lengths[i] * dec_input:size(1)) -- normalize loss by # words in decoded ground truth and batch size
+        loss = loss / torch.sum(out_lengths[i])
         local batch_perplexity = torch.exp(loss)
         data_perplexity = data_perplexity + (batch_perplexity / enc_inputs:size(1))
     end
